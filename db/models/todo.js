369eb20cba6d402dfc7338b933435d6e863ce270
Vitaly <vbshmit@gmail.com>
2021-07-08 21:29:40 +0300

    added virtual id for client side

--- db/models/todo.js
+++ db/models/todo.js
@@ -6,6 +6,12 @@ const TodoSchema = new Schema({
     title: { type: String, required: true },
     isDone: { type: Boolean, required: true, default: false },
     position: { type: String, required: true}
+    },
+    { toJSON: { virtuals: true } }
+);
+
+TodoSchema.virtual("id").get(function () {
+    return this._id;
 });
 
 module.exports = mongoose.model("Todo", TodoSchema);
